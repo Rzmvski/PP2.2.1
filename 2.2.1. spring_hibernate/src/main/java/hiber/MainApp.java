c8020fe5e77d7837e2a9a725dd2f87be1a163268
Rzmvski <victor.razumovski@gmail.com>
2022-12-13 23:41:11 +0300

    Main application was written

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -2,6 +2,8 @@ package hiber;
 
 import hiber.config.AppConfig;
 import hiber.model.User;
+import hiber.model.Car;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,11 +16,21 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
 
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru",
+              new Car("model1", 12345)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru",
+              new Car("model2", 56789)));
+      userService.add(new User("User7", "Lastname7", "user7@mail.ru",
+              new Car("model3", 101112)));
+      userService.add(new User("User8", "Lastname8", "user8@mail.ru",
+              new Car("model4", 131415)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +38,14 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println("Car = "+user.getCar());
       }
+
+      System.out.println(carService.getOwner("model1", 12345));
+      System.out.println(carService.getOwner("model2", 56789));
+      System.out.println(carService.getOwner("model3", 101112));
+      System.out.println(carService.getOwner("model4", 131415));
+
 
       context.close();
    }
